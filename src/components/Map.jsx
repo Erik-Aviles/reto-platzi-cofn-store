45c4e81636574fffa38bc67b70377c6f286ed385
Erika <erik8822@hotmail.com>
2022-12-21 16:11:45 -0500

    con catch (error)

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,28 +1,48 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import axios from "axios";
 import { MapContainer, TileLayer, Marker , Popup} from 'react-leaflet';
 import '../styles/Map.css'
+// import AppContext from '../context/AppContext';
 
 
 
-function Map ({data}) { 
+function Map () { 
     // const defaultPosition =  [51.505, -0.09]
-    const defaultPosition =  {lat: data.latitude, lng: data.longitude}
-    // const defaultPosition =  [data.latitude, data.longitude]
+    const [ubicacion, setUbicacion] = useState([]);
+    // const { state: {buyer}} = useContext(AppContext)
 
-console.log(defaultPosition)
+    const Api = 'http://api.positionstack.com/v1/forward'
+
+    const params = {
+        access_key: 'ad6fd358a8dcfb4085b12127cc30df77',
+        query: 'Riobamba Ecuador'/* buyer[0].city */
+    }
+
+    useEffect(() => {
+        axios(Api, {params})
+            .then(response => {
+                setUbicacion(response.data.data);
+                console.log(response.data.data)
+            }) .catch((error )=> {
+              console.log(error);
+          }); 
+    }, []);
+    console.log()
+
   return (
-    <div className='leaflet-container'>
+     <div className='leaflet-container'>
+     {ubicacion.map(item => (
       <MapContainer 
-        center={defaultPosition} 
+        center={{lat: item.latitude, lng: item.longitude}} 
         zoom={13} 
         scrollWheelZoom={false}
-      >
+      > 
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <Marker 
-          position={defaultPosition}
+          position={{lat: item.latitude, lng: item.longitude}}
           
         >
           
@@ -30,8 +50,9 @@ console.log(defaultPosition)
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
         </Marker>
-      </MapContainer>
+      </MapContainer>))}
     </div>
+
   )
 }
 
